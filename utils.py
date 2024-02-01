2a772b30ffbe308696eadd63e5f4a91350ecbcc2
Pavan Katta <pavanyellow@gmail.com>
2024-02-01 15:33:16 +0530

    a

--- utils.py
+++ utils.py
@@ -3,9 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from dataclasses import dataclass
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import random
 import matplotlib.pyplot as plt
 import time
@@ -17,7 +14,7 @@ import plotly.express as px
 class SAE_Config:
     input_size : int = 5
     hidden_size : int = 50
-    l1_coefficient : int = 0.001
+    l1_coefficient : int = 0.01
 
 
 class SAE(nn.Module):
@@ -41,3 +38,35 @@ class SAE(nn.Module):
 
         return idx, loss, hidden
 
+@dataclass
+class SuperPositionConfig:
+    input_size : int = 20
+    hidden_layer_size: int = 5
+    imp_vector : torch.Tensor = torch.tensor([0.7**i for i in range(input_size)])
+
+
+class SuperpositionModel(nn.Module):
+    def __init__(self, config : SuperPositionConfig):
+        super().__init__()
+        self.config = config
+        self.encoder = nn.Linear(config.input_size, config.hidden_layer_size, bias= False)
+        self.decoder = nn.Linear(config.hidden_layer_size, config.input_size)
+        self.layers = [self.encoder,self.decoder]
+
+        self.decoder.weight.data = self.encoder.weight.data.t()
+
+        self.relu = nn.ReLU()
+    
+
+    def get_loss(self, target, output):
+        loss = (self.config.imp_vector*((target-output)**2)).mean()
+        return loss
+        
+    def forward(self, input, targets = None, return_hidden = False):
+         
+        hidden = self.encoder(input)
+        final = self.decoder(hidden)
+        logits = self.relu(final)
+
+        loss = self.get_loss(targets, logits)
+        return logits, loss, hidden
